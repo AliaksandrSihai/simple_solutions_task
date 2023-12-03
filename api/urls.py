74e8316fbeb34f9c7d3e967897b9d984951cf4f4
Aliaksandr <sigai.aleksandr@mail.ru>
2023-12-03 14:17:39 +0200

    Docker compose

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from api.apps import ApiConfig
+from rest_framework import routers
+from django.urls import path
+from api.views import ItemViewSet, CartRetrieveAPIView
+
+app_name = ApiConfig.name
+
+
+router = routers.DefaultRouter()
+router.register(r'item', ItemViewSet, basename='item')
+
+
+urlpatterns = [
+    path('cart/<int:pk>/', CartRetrieveAPIView.as_view(), name='cart'),
+] + router.urls
